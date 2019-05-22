5c1d8a98c8598f145c9af29efd16cf94fc813ff0
LShad <michal.ratajczak@onet.eu>
2019-05-22 17:37:09 +0200

    Added all new files

---
+++ index.js
@@ -0,0 +1,25 @@
+function timer()
+{
+	var timer = document.getElementById("timer");
+	var today = new Date();
+	timer.innerHTML = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	return today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+}
+	
+function send(event)
+{
+	if (event.type == "click" || event.key == "Enter") 
+	{
+		var typemassage = document.getElementById("typemassage");
+		var typemassagetext = typemassage.value;
+		var name = document.getElementById("name");
+		var nametext = name.value;
+		var showmassage1 = document.getElementById("showmassage");
+		showmassage1.value = showmassage1.value + timer() + " " + nametext + ": " + typemassagetext + "\n";
+		typemassage.value = "";
+		showmassage1.scrollTop = showmassage1.scrollHeight;
+	}
+}
+	
+var idInterwalu = window.setInterval(timer, 1000);
+showmassage.scrollTop = showmassage.scrollHeight; 
